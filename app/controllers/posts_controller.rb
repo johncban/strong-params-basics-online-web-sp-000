4f9a7ef01aabbe30722fb6991a157095b5ba5587
Juan Carlo Banayo <johncban@gmail.com>
2020-05-24 19:23:02 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,7 @@ class PostsController < ApplicationController
 	def update
 	  @post = Post.find(params[:id])
 	  # @post.update(params["post"])
-		@post.update(params.require(:post).permit(:title, :description))
+		@post.update(params.require(:post).permit(:title))
 	  redirect_to post_path(@post)
 	end
 
